ad9b2072a1098a7868d8d13176c8ebbeb397ed90
Susan VanderPlas <srvanderplas@gmail.com>
2013-09-26 11:35:35 -0500

    Updated quadratic approximation

--- functions.R
+++ functions.R
@@ -1,6 +1,7 @@
 library(gridExtra)
 
 createSine <- function(n=200, len=1, f=f, fprime=fprime, f2prime=f2prime, a=0, b=2*pi) {
+  #  if(getquadapprox & !is.function(f2prime)) f2prime <- function(x) -1*f(x) # for backwards compatibility
   x <- seq(a, b, length=n+2)[(2:(n+1))]
   ell <- rep(len, length=length(x))
   fx <- f(x)
@@ -12,7 +13,9 @@ createSine <- function(n=200, len=1, f=f, fprime=fprime, f2prime=f2prime, a=0, b
   dx <- diff(range(x))
   # fprime works in framework of dx and dy, but we represent it in framework of dx and dy+len
   # needs to be fixed by factor a:  
-  a <- dy/(dy + len) 
+  #  a <- dy/(dy + len) 
+  dyl <- diff(range(c(ystart, yend)))
+  a <- dy/(dyl) 
   # ellx is based on the "trig" correction
   ellx <- ell / cos(atan(abs(a*fprime(x))))
   # ellx2 is based on linear approximation of f  
@@ -23,19 +26,41 @@ createSine <- function(n=200, len=1, f=f, fprime=fprime, f2prime=f2prime, a=0, b
   
   # third adjustment is based on quadratic approximation of f.
   # this needs two parts: correction above and below f(x)  
+  #   if(getquadapprox & is.function(f2prime)){
+  #     secseg <- do.call("rbind", lapply(dframe$x, function(i) getSecantSegment(i, dframe, f, fprime, f2prime)))
+  #     dframe$ellx3.u <- secseg$sec.ell1
+  #     dframe$ellx3.l <- secseg$sec.ell2
+  #   }
   
   fp <- a*fprime(x)
   f2p <- a*f2prime(x)
-  lambdap <- (sqrt((fp^2+1)^2-f2p*fp^2*ell) + fp^2 + 1)^-1    
-  lambdam <- -(sqrt((fp^2+1)^2+f2p*fp^2*ell) + fp^2 + 1)^-1    
+  v <- 1 + fp^2
+  lambdapinv <- 0.5*(sqrt(v^2-f2p*fp^2*ell) + v)    
+  #  lambdaminv <- -0.5*(sqrt(v^2+f2p*fp^2*ell) + v)
+  lambdaminv <- 0.5*(sqrt(v^2+f2p*fp^2*ell) + v)
   
+  dframe$ellx4.l <- 0.5*abs(lambdapinv)/sqrt(v)
+  dframe$ellx4.u <- 0.5*abs(lambdaminv)/sqrt(v)
+  #  dframe$lambdam <- 1/lambdaminv
+  #  dframe$lambdap <- 1/lambdapinv
+  fp <- fprime(x)
+  f2p <- f2prime(x)
+  v <- 1 + fp^2
+  #  lambda1 <- (-v + sqrt(v^2 - fp^2*f2p*ell))/(fp^2*f2p) # these two work
+  #  lambda2 <- (-v + sqrt(v^2 + fp^2*f2p*ell))/(fp^2*f2p) # these two work
   
-  dframe$ellx4.l <- (4*abs(lambdap)*sqrt(1+fp^2))^-1
-  dframe$ellx4.u <- (4*abs(lambdam)*sqrt(1+fp^2))^-1
+  lambdaa <- -2*(sqrt(v^2 - f2p*fp^2*ell) + v)^-1 # identical to top
+  lambdab <- 2*(sqrt(v^2 + f2p*fp^2*ell) + v)^-1
+  
+  dframe$lambdam <- lambdaa
+  dframe$lambdap <- lambdab
+  
+  # qplot(a*(sqrt(1 + a^2*fprime(x)^2)*abs(lambdab))^-1, ellx4.u, data=dframe)
+  # qplot(a*(sqrt(1 + a^2*fprime(x)^2)*abs(lambdaa))^-1, ellx4.l, data=dframe)
+  # ggplot(aes(x, y+ell*(a*sqrt(v)*abs(lambdaa))^-1, xend=x, yend=y-ell*(a*sqrt(v)*abs(lambdab))^-1), data=dframe) + geom_segment()  
   
   dframe
 }
-
 
 correctx <- function(z, fprime, a=-pi, b=pi, w=1) {
   # w = 1/(shrink+1)
